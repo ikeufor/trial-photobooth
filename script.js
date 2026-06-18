ada1fa951dbd7bcef3c2ef0a799cb5bf76a28d1a
ikeufor <ikeufor@gmail.com>
2026-06-18 23:30:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,11 +33,11 @@ canvas.height = 1177
 // PHOTO AREA
 // =========================
 
-const PHOTO_X = 50
-const PHOTO_Y = 102
+const PHOTO_X = 50;
+const PHOTO_Y = 102;
 
-const PHOTO_WIDTH = 660
-const PHOTO_HEIGHT = 870
+const PHOTO_WIDTH = 659;
+const PHOTO_HEIGHT = 870;
 
 // =========================
 // FRAME
